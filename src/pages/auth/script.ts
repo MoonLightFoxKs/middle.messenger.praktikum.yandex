b3ec17ca8fa4be5148c6f9f6859bea498d506f09
Котова Ксения Дмитриевна <kkotova@alfabank.ru>
2023-12-01 18:13:38 +0300

    fix(*): prettier ts

--- src/pages/auth/script.ts
+++ src/pages/auth/script.ts
@@ -1,13 +1,17 @@
-function handleClick(divId:string) {
-    const element = document.getElementById(divId);
-    const hidden = element?.getAttribute("style");
+function handleClick(divId: string) {
+  const element = document.getElementById(divId);
+  const hidden = element?.getAttribute("style");
 
-    if (hidden) {
-        element?.removeAttribute("style");
-    } else {
-        element!.style.display = 'none';
-    }
+  if (hidden) {
+    element?.removeAttribute("style");
+  } else {
+    element!.style.display = "none";
+  }
 }
 
-document.getElementById("singinText")?.addEventListener("click",()=>handleClick("signin"))
-document.getElementById("loginText")?.addEventListener("click",()=>handleClick("signin"))
+document
+  .getElementById("singinText")
+  ?.addEventListener("click", () => handleClick("signin"));
+document
+  .getElementById("loginText")
+  ?.addEventListener("click", () => handleClick("signin"));
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import pugPlugin from "vite-plugin-pug"
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
 
-import { resolve } from 'path'
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [pugPlugin()],
@@ -10,20 +10,20 @@ export default defineConfig({
       less: {
         math: "always",
         relativeUrls: true,
-        javascriptEnabled: true
+        javascriptEnabled: true,
       },
     },
   },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'src/pages/auth/index.html'),
-        chats: resolve(__dirname, 'src/pages/chat/index.html'),
-        profile: resolve(__dirname, 'src/pages/profile/index.html'),
-        page404: resolve(__dirname, 'src/pages/404/index.html'),
-        page500: resolve(__dirname, 'src/pages/500/index.html'),
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "src/pages/auth/index.html"),
+        chats: resolve(__dirname, "src/pages/chat/index.html"),
+        profile: resolve(__dirname, "src/pages/profile/index.html"),
+        page404: resolve(__dirname, "src/pages/404/index.html"),
+        page500: resolve(__dirname, "src/pages/500/index.html"),
       },
-    }
-  }
-})
+    },
+  },
+});
